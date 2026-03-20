050be06535940ede941f834017b5784106ce1af4
KRISH826 <krishnendupanja98@gmail.com>
2026-03-21 01:35:07 +0530

    fix: correct authorization middleware for fetching all categories

--- backend/src/routes/category.route.ts
+++ backend/src/routes/category.route.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post("/", requireAuth, requireRole("admin"), CategoryController.createCategoryController);
 router.get("/:id", requireAuth, requireRole("admin", "customer"), CategoryController.getCategoryByIdController);
-router.get("/", requireAuth, CategoryController.getAllCategoriesController);
+router.get("/", requireRole("admin", "customer"),CategoryController.getAllCategoriesController);
 router.put("/:id", requireAuth, requireRole("admin"), CategoryController.updateCategoryController);
 router.delete("/:id", requireAuth, requireRole("admin"), CategoryController.deleteCategoryController);
 
