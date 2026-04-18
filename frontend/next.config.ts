af50aca33a8159d522186d2a3b6e89e05f2f36b1
KRISH826 <krishnendupanja98@gmail.com>
2026-04-19 01:53:22 +0530

    okay clouflare remove

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -18,14 +18,14 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/api/:path*",
-  //       destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
