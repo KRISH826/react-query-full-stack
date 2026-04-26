407fb4746b1536ffbb8aa4b937bcc8967bcd978e
KRISH826 <krishnendupanja98@gmail.com>
2026-04-26 14:05:18 +0530

    refactor: remove RequireAuth and GuestOnly components from protected pages

--- frontend/app/(main)/(protected)/favourites/page.tsx
+++ frontend/app/(main)/(protected)/favourites/page.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import FavouritePage from './_components/FavouritePage'
-import RequireAuth from '@/components/auth/RequireAuth'
 
 const page = () => {
     return (
-        <RequireAuth>
             <div>
                 <FavouritePage />
             </div>
-        </RequireAuth>
     )
 }
 
--- frontend/app/(main)/(protected)/orders/[id]/page.tsx
+++ frontend/app/(main)/(protected)/orders/[id]/page.tsx
@@ -1,10 +1,8 @@
-import RequireAuth from "@/components/auth/RequireAuth";
+
 
 const page = () => {
     return (
-        <RequireAuth>
             <div>Order Details</div>
-        </RequireAuth>
     )
 }
 
