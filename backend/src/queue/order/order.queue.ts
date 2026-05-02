8667a661493f32fb23241d91e40cd2314ee1245f
KRISH826 <krishnendupanja98@gmail.com>
2026-05-02 16:21:21 +0530

    order backend fix

--- backend/src/queue/order/order.queue.ts
+++ backend/src/queue/order/order.queue.ts
@@ -1,31 +1,27 @@
+// queue/order/order.queue.ts
+
 import { Queue } from "bullmq";
-import { CreateOrderDTO, DirectPurchaseDTO } from "../../models/order";
 import { redisConnection } from "../../db/redis";
 
-export interface CartOrderData {
-    type: "cart";
-    userId: string;
-    orderData: CreateOrderDTO
-}
-
-export interface BuyNowOrderData {
-    type: "buy-now";
-    productId: string;
-    orderData: DirectPurchaseDTO
-    userId: string;
-}
-
-export type OrderData = CartOrderData | BuyNowOrderData;
-
-export const orderQueue = new Queue<OrderData>("order-queue", {
-    connection: redisConnection,
-    defaultJobOptions: {
-        attempts: 3,
-        backoff: {
-            type: "exponential",
-            delay: 2000
-        },
-        removeOnComplete: 1000,
-        removeOnFail: 500
+export type OrderEventData =
+  | {
+      type: "order.confirmed";
+      orderId: string;
     }
+  | {
+      type: "order.failed";
+      orderId: string;
+    };
+
+export const orderQueue = new Queue<OrderEventData>("order-events-queue", {
+  connection: redisConnection,
+  defaultJobOptions: {
+    attempts: 5,
+    backoff: {
+      type: "exponential",
+      delay: 2000,
+    },
+    removeOnComplete: 1000,
+    removeOnFail: 500,
+  },
 });
