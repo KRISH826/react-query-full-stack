1220c24188399041d7a0a4706fb8c5cbf0cd8f46
KRISH826 <krishnendupanja98@gmail.com>
2026-05-30 18:44:49 +0530

    brevo set up

---
+++ backend/src/utils/brevo.ts
@@ -0,0 +1,22 @@
+import {BrevoClient} from "@getbrevo/brevo";
+import { config } from "../config/config";
+
+const brevo = new BrevoClient({apiKey: config.brevo.api_key!})
+
+export const sendEmail = async ( to: string | string[],
+  subject: string,
+  html: string): Promise<void> => {
+    const recipients = (Array.isArray(to) ? to : [to]).map((email) => ({ email }));
+
+    await brevo.transactionalEmails.sendTransacEmail({
+        sender: {
+            email: config.brevo.email_form,
+            name: config.brevo.sender,
+        },
+        to: recipients,
+    subject,
+    htmlContent: html,
+    });
+
+    console.log("[Brevo] Email sent successfully to:", to);
+}
